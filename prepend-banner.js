372acd12aff49be4f3fb647062eb30b717d62186
Kyle Phillips <kyle@haptic-data.com>
2015-08-23 23:33:15 -0700

    new build command, remove karma, mocha, grunt, add browserify and uglify-js from devDependencies

---
+++ prepend-banner.js
@@ -0,0 +1,19 @@
+var pkg = require('./package.json');
+
+var date = new Date();
+
+var banner = '// Animitter ' +pkg.version+ '\n' +
+             '// Build: ' + date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate() + '\n' +
+             '// by [Kyle Phillips](http://haptic-data.com)\n' +
+             '// Available under [MIT License](http://github.com/hapticdata/animitter/blob/master/LICENSE)\n' +
+             '// Env: Browser + Node\n';
+
+process.stdin.setEncoding('utf8');
+
+process.stdout.write(banner);
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(chunk);
+  }
+});
